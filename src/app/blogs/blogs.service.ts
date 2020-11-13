9c420e062e7432dbda21fbb637e5a6e161375b04
patlepranay <pranayhpatle@gmail.com>
2020-11-13 12:12:38 +0530

    updated https

--- src/app/blogs/blogs.service.ts
+++ src/app/blogs/blogs.service.ts
@@ -3,26 +3,29 @@ import { HttpClient } from "@angular/common/http";
 import { Subject } from "rxjs";
 import { map } from 'rxjs/operators';
 
-import { environment } from "../../environments/environment"
+import { environment } from "../../environments/environment";
+
 const BACKEND_URL = environment.apiUrl;
+
 @Injectable({ providedIn: "root" })
 export class BlogsService {
-  constructor(private http: HttpClient) {}
-    blogs;
-  getBlogs():any {
+  constructor(private http: HttpClient) { }
+  blogs;
+
+  getBlogs(): any {
     this.http
-      .get(BACKEND_URL+"/blogs"
+      .get("https://krishi-sarthi.herokuapp.com/blogs"
       ).subscribe(transformedBlogs => {
-          this.blogs=Object(transformedBlogs)["blogs"]
+        this.blogs = Object(transformedBlogs)["blogs"]
         //   console.log(this.blogsData);
       });
-      return this.blogs;
+    return this.blogs;
   }
 
-  addBlog(author: string, title: string,body:string) {
-    const blog  = { id: null, title: title, body: body,author:author };
+  addBlog(author: string, title: string, body: string) {
+    const blog = { id: null, title: title, body: body, author: author };
     this.http
-      .post(BACKEND_URL+"/blogs/request", blog)
+      .post("https://krishi-sarthi.herokuapp.com/blogs/request", blog)
       .subscribe(responseData => {
         console.log(responseData);
       });
